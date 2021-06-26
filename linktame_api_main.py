1844bc10e5a7e7d1d07cda33fa4a2a24ecb9b734
richarda308 <richarda@getstrut.com>
2021-06-27 03:53:11 +1000

    Fixed JWT encode in create_user

--- linktame_api_main.py
+++ linktame_api_main.py
@@ -190,7 +190,7 @@ def create_user():
         return jsonify({'successful' : 'false', 'message' : 'Invalid signup data!'}), 400
 
     #Create token that is active for timedelta period. datetime needs to be in unix utc timestamp format
-    token = jwt.encode({'public_id' : email.public_id, 'email' : email.email, 'name' : email.name, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'], algorithm="HS256")
+    token = jwt.encode({'public_id' : new_user.public_id, 'email' : new_user.email, 'name' : new_user.name, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'], algorithm="HS256")
 
     return jsonify({'successful' : 'true', "message" : "User Created!", 'token' : token}), 200
 
